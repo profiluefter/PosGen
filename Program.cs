692079105df6e7778c4a2ff5673e035c93405aa3
Fabian <fabian.paul.gurtner@gmail.com>
2019-11-21 22:01:27 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PosGen
+{
+    class Program
+    {
+        private static string[] menuItems = {"One", "Two"};
+
+        private static int selection = 0;
+        
+        static void Main(string[] args)
+        {
+            initialWrite();
+        }
+
+        private static void initialWrite()
+        {
+            var originalBackgroundColor = Console.BackgroundColor;
+            var originalForegroundColor = Console.ForegroundColor;
+            for (var i = 0; i < menuItems.Length; i++)
+            {
+                if (selection == i)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                
+                Console.WriteLine(i+") "+menuItems[i]);
+                
+                if (selection == i)
+                {
+                    Console.BackgroundColor = originalBackgroundColor;
+                    Console.ForegroundColor = originalForegroundColor;
+                }
+            }
+        }
+
+        private static void updateMenuOption(int i)
+        {
+            //TODO: Write i. line again
+        }
+    }
+}
